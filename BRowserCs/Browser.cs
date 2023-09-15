b2903905953c351230f77c70eec85f1f2eed5ff5
opilane <opilane@HP-CZC2349HTN>
2023-09-15 08:42:40 +0300

    Add project files.

---
+++ BRowserCs/Browser.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRowserCs
+{
+    public class Browser
+    {
+        private Stack<string> _back = new Stack<string>();
+        private Stack<string> _forward = new Stack<string>();
+        private List<string> _history = new List<string>();
+
+        private string _current;
+        private string _homepage;
+
+        public Browser(string homepage)
+        {
+            _homepage = homepage;
+            Console.WriteLine("Your home page -> ");
+            GoTo(homepage);
+        }
+
+        public string Homepage
+        {
+            get => _homepage;
+            set => _homepage = value;
+        }
+        public static void Change_homepage(Browser br)
+        {
+            Console.WriteLine("Write new homepage");
+            string nhomepage = Console.ReadLine();
+            br.Homepage = nhomepage;
+            Console.WriteLine("Your home page -> ");
+            br.GoTo(nhomepage);
+        }
+        public static void go_to_honepage(Browser br)
+        {
+            Console.WriteLine("Your home page -> {0} ",br.Homepage);
+            br.GoTo(br.Homepage);
+        }
+
+        public void GoTo(string url)
+        {
+            _back.Push(_current);
+            _current = url;
+            _forward.Clear();
+            _history.Add(url);
+        }
+
+        public void Back()
+        {
+            if (_back.Count > 0)
+            {
+                _forward.Push(_current);
+                _current = _back.Pop();
+                _history.Add(_current);
+            }
+        }
+
+        public void Forward()
+        {
+            int howmanylinks = _forward.Count;
+
+            if (_forward.Count > 0)
+            {
+                _back.Push(_current);
+                _current = _forward.Pop();
+                _history.Add(_current);
+            }
+        }
+
+        public string Current()
+        {
+            return _current;
+        }
+
+        public List<string> History()
+        {
+            return _history;
+        }
+    }
+}
---
+++ BRowserCs/Program.cs
@@ -0,0 +1,95 @@
+﻿using BRowserCs;
+
+public class Program
+{
+    public static void Main()
+    {
+        Browser operagx = new Browser("artursuskevits.wordpress.com");
+        Console.WriteLine(operagx.Current());
+        while (true)
+        {
+            
+            Console.WriteLine("Choose your move\n1-Go to new page\n2-Go back\n3-Go forward\n4-Check browser history\n5-Update homepage\n6-Go to homepage\nElse-Stop working");
+            string youser_answer = Console.ReadLine();
+            if (youser_answer == "1")
+            {
+                Console.WriteLine("Write new link");
+                string newlink = Console.ReadLine();
+                operagx.GoTo(newlink);
+                Console.WriteLine(operagx.Current());
+            }
+            if (youser_answer == "2")
+            {
+                operagx.Back();
+                Console.WriteLine(operagx.Current());
+            }
+            if (youser_answer == "3")
+            {
+                operagx.Forward();
+                Console.WriteLine(operagx.Current());
+            }
+            if (youser_answer == "4")
+            {
+                Console.WriteLine(string.Join(", ", operagx.History()));
+            }
+            if (youser_answer == "5")
+            {
+                Browser.Change_homepage(operagx);
+            }
+            if (youser_answer=="6")
+            {
+                Browser.go_to_honepage(operagx);
+            }
+        }
+
+        //Browser browser = new Browser();
+        //browser.GoTo("a");
+        //browser.GoTo("b");
+        //browser.GoTo("c");
+        //browser.Back();
+        //browser.Back();
+        //browser.Back();
+        //Console.WriteLine(browser.Current());
+        //browser.Forward();
+        //Console.WriteLine(browser.Current());
+        //browser.Forward();
+        //Console.WriteLine(browser.Current());
+
+        //Console.WriteLine(string.Join(", ", browser.History()));
+        //// google.com, a, b, c, b, a, google.com, a, b
+
+        //// ----
+        //browser = new Browser();
+        //Console.WriteLine(browser.Current());
+        //browser.GoTo("facebook.com");
+        //Console.WriteLine(browser.Current());
+        //browser.GoTo("google.com");
+        //Console.WriteLine(browser.Current());
+        //browser.Back();
+        //Console.WriteLine(browser.Current());
+        //browser.Forward();
+        //Console.WriteLine(browser.Current());
+        //browser.GoTo("neti.ee");
+        //Console.WriteLine(browser.Current());
+
+        //Console.WriteLine(string.Join(", ", browser.History()));
+
+        //// google.com, facebook.com, google.com, facebook.com, google.com, neti.ee
+
+        //// ----
+
+        //browser = new Browser();
+        //browser.GoTo("neti.ee");
+        //browser.GoTo("facebook.com");
+        //browser.Back();
+        //browser.Back();
+        //browser.GoTo("bing.com");
+        //browser.Forward();
+        //browser.Forward();
+        //Console.WriteLine(browser.Current());
+
+        //Console.WriteLine(string.Join(", ", browser.History()));
+        //// google.com, neti.ee, facebook.com, neti.ee, google.com, bing.com
+        ////Console.
+    }
+}
